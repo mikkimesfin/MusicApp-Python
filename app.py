0426c654fbe4bdcbd81779e0f67083ffea2718e6
Mikki Mesfin <mikki.mesfin@gmail.com>
2017-09-26 20:03:25 -0600

    get request done 💪

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-import sqlite3
+import sqlite3, json
 from flask import Flask, request, jsonify
 
 
@@ -8,7 +8,9 @@ app = Flask(__name__)
 @app.route('/api/songs', methods=['GET', 'POST'])
 def collection():
     if request.method == 'GET':
-        pass  # Handle GET all Request
+        # Handle GET all Request
+        all_songs = get_all_songs()
+        return json.dumps(all_songs)
     elif request.method == 'POST':
         # Handle POST request
         data = request.form
@@ -39,6 +41,13 @@ def add_song(artist, title, rating):
     except:
         result = {'status': 0, 'message': 'error'}
     return result
+
+def get_all_songs():
+    with sqlite3.connect('songs.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM songs ORDER BY id desc")
+        all_songs = cursor.fetchall()
+        return all_songs
 
 if __name__ == '__main__':
     app.debug = True
