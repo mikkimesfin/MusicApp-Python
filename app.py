dc92629e7a511c059fb3f875c8abab1707a22536
Mikki Mesfin <mikki.mesfin@gmail.com>
2017-09-26 20:06:39 -0600

    get a single song 🎻

--- app.py
+++ app.py
@@ -22,7 +22,9 @@ def collection():
 @app.route('/api/song/<song_id>', methods=['GET', 'PUT', 'DELETE'])
 def resource(song_id):
     if request.method == 'GET':
-        pass  # Handle GET single request
+        # Handle GET single request
+        song = get_single_song(song_id)
+        return json.dumps(song)
     elif request.method == 'PUT':
         pass  # Handle UPDATE request
     elif request.method == 'DELETE':
@@ -49,6 +51,12 @@ def get_all_songs():
         all_songs = cursor.fetchall()
         return all_songs
 
+def get_single_song(song_id):
+    with sqlite3.connect('songs.db') as connection:
+        cursor = connection.cursor()
+        cursor.execute("SELECT * FROM songs WHERE id = ?", (song_id,))
+        song = cursor.fetchone()
+        return song
 if __name__ == '__main__':
     app.debug = True
     app.run()
